a0c1d06269de43810d84b7970b48c2542c9ce65c
rtgr <mrant783@gmail.com>
2026-09-07 22:57:28 +0500

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/myskidka/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+      },
+    },
+  },
 });
